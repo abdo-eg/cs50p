58d46a1605bb6eaf268ee19c9164f3a38df39597
abdo-eg <alsharifegy@gmail.com>
2024-10-08 11:00:35 +0300

    add all week0 tasks

---
+++ week0/faces.py
@@ -0,0 +1,8 @@
+str = input()
+if ":)" in str:
+    str=str.replace(':)','🙂')
+if ":(" in str:
+    str=str.replace(':(','🙁')
+print(str)
+#we also can use Unicode Characters!
+
---
+++ week0/playback.py
@@ -0,0 +1,14 @@
+# ******* using loops *******
+# str = input()
+# new_str=''
+# for c in str:
+#     if c ==' ':
+#         c='...'
+#         new_str+=c
+#     else:
+#         new_str+=c
+
+# print(new_str)
+# ******* or using one line of code! *******
+print(input().replace(' ','...'))
+
