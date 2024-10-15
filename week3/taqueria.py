4cbdfba1cd23dc4a47e219e7e374bb073c4c7211
abdo-eg <alsharifegy@gmail.com>
2024-10-15 15:00:32 +0300

    taqueria using if condition

---
+++ week3/taqueria.py
@@ -0,0 +1,21 @@
+# link of the task: [https://cs50.harvard.edu/python/2022/psets/3/taqueria/#felipes-taqueria]
+menu ={
+    "baja taco": 4.25,
+    "burrito": 7.50,
+    "bowl": 8.50,
+    "nachos": 11.00,
+    "quesadilla": 8.50,
+    "super burrito": 8.50,
+    "super quesadilla": 9.50,
+    "taco": 3.00,
+    "tortilla salad": 8.00
+}
+total = 0
+try:
+    while True:
+        item=input('Item: ').lower()
+        if item in menu:
+            total += menu[item]
+            print(f'Total: ${total:.2f}')
+except EOFError:
+    print(f'Total: ${total:.2f}')
