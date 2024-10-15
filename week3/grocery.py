3c6cd357aca014a2d07e4c8e22c8299819b35a35
abdo-eg <alsharifegy@gmail.com>
2024-10-15 16:42:35 +0300

    add grocery task

---
+++ week3/grocery.py
@@ -0,0 +1,15 @@
+#link of the task : [https://cs50.harvard.edu/python/2022/psets/3/grocery/]
+orders={}
+
+try :
+    while True:
+        order = input('').upper()
+        if order in orders:
+            orders[order]+=1
+        else:
+            orders[order]=1
+except EOFError:
+    orders= dict(sorted(orders.items()))
+    for order in orders:
+        print(orders[order],order)
+
