4eab8f793a61dbae643677715bd5fbec0a140685
abdo-eg <alsharifegy@gmail.com>
2024-10-22 03:45:40 +0300

    solve adieu.py

---
+++ week4/adieu.py
@@ -0,0 +1,20 @@
+import inflect
+
+def main():
+
+    p = inflect.engine()
+
+    names = []
+
+    try:
+        while True:
+            name= input("Name: ")
+            names.append(name)
+    except EOFError:
+        if len(names)==1:
+            print(f"Adieu, adieu, to {names[0]}")
+        else:
+            print(f"Adieu, adieu, to {p.join(names)}")
+if __name__ == "__main__":
+    main()
+
