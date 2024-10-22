ffb9e344a19e2901238c3b64a4f689516a32aff8
abdo-eg <alsharifegy@gmail.com>
2024-10-22 08:45:27 +0300

    solve bitcoin.py

---
+++ week4/bitcoin.py
@@ -0,0 +1,18 @@
+import sys
+import requests
+try:
+    quantity = float(sys.argv[1])
+except:
+    if len(sys.argv) ==1:
+        sys.exit("Missing command-line argument")
+    else:
+        sys.exit("Command-line argument is not a number")
+# API
+r = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+response = r.json()
+usd = response["bpi"]["USD"]["rate"]
+# remove " , "
+usd = usd.replace(",","")
+total = float(usd) * quantity
+print(f"${total:,.4f}")
+
